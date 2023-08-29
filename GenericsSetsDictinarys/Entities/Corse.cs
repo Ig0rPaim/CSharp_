16f79f3dc756c20fdb6dc3350ee1bafaf945d123
Igor Paim de Oliveira <igorpaimdeoliveira@gmail.com>
2023-08-29 16:07:01 -0300

    studie

---
+++ GenericsSetsDictinarys/Entities/Corse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsSetsDictinarys.Entities
+{
+    internal class Corse
+    {
+        public string NameOfCorse { get; set; }
+        public int NUmberOfStutends { get; set; }
+    }
+}
---
+++ GenericsSetsDictinarys/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsSetsDictinarys.Entities
+{
+    internal class User
+    {
+        public int Code { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Code.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is User)) return false;
+            User other = obj as User;
+            return Code.Equals(other);
+        }
+    }
+}
